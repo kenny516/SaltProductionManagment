96aa87b69757ea84673f18f1b0fee58c76de14a1
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2024-10-23 11:08:51 +0300

    init project

---
+++ src/main/java/com/analytique/gestion_analytique/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.analytique.gestion_analytique;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GestionAnalytiqueApplication.class);
+	}
+
+}
---
+++ src/test/java/com/analytique/gestion_analytique/GestionAnalytiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analytique.gestion_analytique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAnalytiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
