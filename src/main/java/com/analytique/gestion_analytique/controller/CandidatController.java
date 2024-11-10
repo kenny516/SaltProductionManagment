bc1f41d8c0d00dd9ca900666b76fb27153eb54bd
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2024-11-10 12:13:32 +0300

    basic controllers

---
+++ src/main/java/com/analytique/gestion_analytique/controller/CandidatController.java
@@ -0,0 +1,10 @@
+package com.analytique.gestion_analytique.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/candidat")
+public class CandidatController {
+	
+}
