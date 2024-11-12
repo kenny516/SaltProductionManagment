8c45bfe56ae1905d84f8a7ad25f265244beead4c
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2024-11-12 08:42:56 +0300

    Update EmployeController.java

--- src/main/java/com/analytique/gestion_analytique/controller/EmployeController.java
+++ src/main/java/com/analytique/gestion_analytique/controller/EmployeController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.analytique.gestion_analytique.Models.Employe;
 import com.analytique.gestion_analytique.Services.EmployeService;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping("/api/employe")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmployeController {
 
 	EmployeService employeService;
