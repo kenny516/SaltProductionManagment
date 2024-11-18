a20968419d3e6d18161777b5ef6bbd2b457bc759
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2024-11-18 09:00:24 +0300

    setup for postuler

--- src/main/java/com/analytique/gestion_analytique/controller/DiplomeController.java
+++ src/main/java/com/analytique/gestion_analytique/controller/DiplomeController.java
@@ -2,6 +2,7 @@ package com.analytique.gestion_analytique.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.analytique.gestion_analytique.Repositories.DiplomesRepo;
 
 @RestController
 @RequestMapping("/api/diplomes")
+@CrossOrigin("http://localhost:3000")
 public class DiplomeController {
 
 	DiplomesRepo diplomesRepo;
