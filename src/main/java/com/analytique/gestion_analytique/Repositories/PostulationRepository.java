d6006db67ed062fd6930f0b052ec4c1167e9bf5f
nekena123 <rakotomanjatonekena@gmail.com>
2024-11-18 09:00:24 +0300

    modif

--- src/main/java/com/analytique/gestion_analytique/Repositories/PostulationRepository.java
+++ src/main/java/com/analytique/gestion_analytique/Repositories/PostulationRepository.java
@@ -6,15 +6,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface PostulationRepository extends JpaRepository<Postulation, Long> {
 	Optional<Postulation> findByCandidatIdAndStatus(Integer candidatId, String status);
 
 	// Méthode pour trouver les postulations retenues pour un poste donné
 	List<Postulation> findByPosteIdAndStatus(Integer posteId, String status);
-
-	
 
 }
