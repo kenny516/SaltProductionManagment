45440343db2ce807a5a36dca6008dccc4afeb0c9
nekena123 <rakotomanjatonekena@gmail.com>
2024-12-01 11:33:24 +0300

    change

--- src/main/java/com/analytique/gestion_analytique/Services/OffreEmploiService.java
+++ src/main/java/com/analytique/gestion_analytique/Services/OffreEmploiService.java
@@ -2,13 +2,20 @@ package com.analytique.gestion_analytique.Services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.analytique.gestion_analytique.Models.OffreEmploi;
 import com.analytique.gestion_analytique.Repositories.OffreEmploiRepository;
 import com.analytique.gestion_analytique.dto.receive.OffreEmploiRecieve;
 
+@Service
 public class OffreEmploiService {
 
-    OffreEmploiRepository oeRepository;
+    private OffreEmploiRepository oeRepository;
+
+    public OffreEmploiService(OffreEmploiRepository oeRepository) {
+        this.oeRepository = oeRepository;
+    }
 
     public List<OffreEmploi> getAll() {
         return oeRepository.findAll();
