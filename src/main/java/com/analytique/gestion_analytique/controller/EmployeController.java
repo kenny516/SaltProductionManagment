27a0401e2587eedb6e5b250c58e4ed0c9731ea90
El <irina.elina.r@gmail.com>
2024-12-02 21:42:38 +0300

    Fetch all categories personnel

--- src/main/java/com/analytique/gestion_analytique/controller/EmployeController.java
+++ src/main/java/com/analytique/gestion_analytique/controller/EmployeController.java
@@ -1,18 +1,22 @@
 package com.analytique.gestion_analytique.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.analytique.gestion_analytique.Models.CategoriePersonnel;
+import com.analytique.gestion_analytique.Repositories.CategoriePersonnelRepository;
 import com.analytique.gestion_analytique.Services.EmployeService;
 import com.analytique.gestion_analytique.dto.send.EmployeSend;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/employe")
@@ -20,9 +24,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class EmployeController {
 
 	EmployeService employeService;
+	CategoriePersonnelRepository categoriePersonnelRepository;
 
-	public EmployeController(EmployeService employeService, JdbcTemplate jdbcTemplate) {
+	public EmployeController(EmployeService employeService, CategoriePersonnelRepository categoriePersonnelRepository) {
 		this.employeService = employeService;
+		this.categoriePersonnelRepository = categoriePersonnelRepository;
 	}
 
 	@GetMapping("")
@@ -43,4 +49,23 @@ public class EmployeController {
 	public List<EmployeSend> getEmployeByPoste(@PathVariable Integer id) {
 		return employeService.getQualifiedEmployeesForPost(id);
 	}
+
+	@GetMapping("/categories-personnel")
+	public List<CategoriePersonnel> getCategoriesPersonnel() {
+		List<CategoriePersonnel> fetched = categoriePersonnelRepository.findAll();
+
+		List<CategoriePersonnel> categs = new ArrayList<>();
+		
+		CategoriePersonnel tous = new CategoriePersonnel();
+		tous.setId(0);
+		tous.setNom("Tous");
+		tous.setDescription("Toutes les catégories de personnel");
+
+		// categs.add(tous);
+
+		categs.addAll(fetched);
+
+		return categs;
+	}
+	
 }
