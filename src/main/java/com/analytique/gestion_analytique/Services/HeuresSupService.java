fe3e94389d7adac3495a872d8fae0cfbbb8300f6
nekena123 <rakotomanjatonekena@gmail.com>
2024-12-02 15:45:17 +0300

    Update HeuresSupService.java

--- src/main/java/com/analytique/gestion_analytique/Services/HeuresSupService.java
+++ src/main/java/com/analytique/gestion_analytique/Services/HeuresSupService.java
@@ -65,28 +65,6 @@ public class HeuresSupService {
         // Vérifier si la date donnée est un jour férié
         return joursFeriesFixes.contains(jour);
     }
-    
-    private LocalDate getLundiDePâques(int annee) {
-        // Calcul du dimanche de Pâques selon l'algorithme de Gauss
-        int a = annee % 19;
-        int b = annee / 100;
-        int c = annee % 100;
-        int d = b / 4;
-        int e = b % 4;
-        int f = (b + 8) / 25;
-        int g = (b - f + 1) / 3;
-        int h = (19 * a + b - d - g + 15) % 30;
-        int i = c / 4;
-        int k = c % 4;
-        int l = (32 + 2 * e + 2 * i - h - k) % 7;
-        int m = (a + 11 * h + 22 * l) / 451;
-        int mois = (h + l - 7 * m + 114) / 31;
-        int jour = ((h + l - 7 * m + 114) % 31) + 1;
-    
-        // Le lundi de Pâques est le lendemain du dimanche de Pâques
-        return LocalDate.of(annee, mois, jour).plusDays(1);
-    }
-
 
     public HeuresSup creerHeureSup(HeuresSup heureSup) {
 
