dc31e6dcfc4eb3278b5cf4336b8877e63486733d
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2024-12-02 11:24:36 +0300

    Update TypeConge.java
    nanala import tsy ilaina

--- src/main/java/com/analytique/gestion_analytique/Models/TypeConge.java
+++ src/main/java/com/analytique/gestion_analytique/Models/TypeConge.java
@@ -4,10 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
 @Data
 @Entity
