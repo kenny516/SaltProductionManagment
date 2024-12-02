be511f332f20e9aadbe35e9e6c812d21b050681e
nekena123 <rakotomanjatonekena@gmail.com>
2024-12-02 15:57:21 +0300

    Update HeuresSupRepository.java

--- src/main/java/com/analytique/gestion_analytique/Repositories/HeuresSupRepository.java
+++ src/main/java/com/analytique/gestion_analytique/Repositories/HeuresSupRepository.java
@@ -4,9 +4,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.analytique.gestion_analytique.Models.HeuresSup;
 
-public interface HeuresSupRepository extends JpaRepository<HeuresSup, Long> {
+public interface HeuresSupRepository extends JpaRepository<HeuresSup, Integer> {
+    @Query(value="SELECT * FROM HeuresSup h " +
+       "WHERE h.id_employe = :idEmploye " +
+       "AND EXTRACT(MONTH FROM h.date_debut) = :mois " +
+       "AND EXTRACT(YEAR FROM h.date_debut) = :annee", nativeQuery=true)
+    List<HeuresSup> findByEmployeAndMonthAndYear(@Param("idEmploye") Long idEmploye, @Param("mois") int mois, @Param("annee") int annee);
 
     @Query("SELECT h FROM HeuresSup h WHERE h.dateDebut >= :today")
     List<HeuresSup> findHeuresSupAfterToday(LocalDateTime today);
